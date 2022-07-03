b9955f9af0c9df6f9bd18884a55f1012d1119ddb
lmh <tnwtw@naver.com>
2022-07-03 19:06:21 +0900

    class

---
+++ class_car.py
@@ -0,0 +1,27 @@
+class Car():
+    instance_count=0
+
+    def __init__(self,color,speed,size):
+        self.color=color
+        self.speed=speed
+        self.size=size
+        Car.instance_count+=1
+        print(f'객체수:{Car.instance_count}')
+    def move(self):
+        self.speed+=10
+
+car1=Car('black',0,'big')
+car2=Car('red',20,'small')
+car3=Car('red',20,'small')
+
+i=0
+while True:
+    i+=1
+    car1.move()
+    if i>5:
+        break
+    
+
+print(car1.speed)
+
+print(f'class의 총 인스턴스 개수:{car1.instance_count}')
